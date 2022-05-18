5da3cf338a518ea7bbc48faf3dbc32fe91d2e496
Nowshad Ahmed <nowshad.ahmed@hotmail.co.ukgit config user.name Nowshad10>
2022-05-18 11:08:50 +0100

    Alex made index template

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from flask import Flask, jsonify, render_template
+from crypt import methods
+import re
+from urllib import request
+from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
 from werkzeug import exceptions
+from controllers import urls
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
+@app.route('/', methods = ['GET'])
 def home():
-    return '<h1>Welcome to the URL Shortener</h1>'
+    if request.method == 'GET':
+        return render_template('index.html')
+
+@app.route('/all', methods = ['GET'])
+def all():
+    all_urls = urls.get_all()
+    return jsonify(all_urls)
 
 if __name__ == '__main__':
     app.run(debug=True)
